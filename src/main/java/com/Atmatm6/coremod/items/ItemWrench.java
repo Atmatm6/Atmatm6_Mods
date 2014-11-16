e81d674c7b5f301d6c02b80d3fdaa3ca9a644460
Atmatm6 Yolo <atmatm6000@gmail.com>
2014-11-15 20:13:00 -0500

    src

---
+++ src/main/java/com/Atmatm6/coremod/items/ItemWrench.java
@@ -0,0 +1,11 @@
+package com.Atmatm6.coremod.items;
+
+/**
+ * Created by Austin on 2014-11-11.
+ */
+public class ItemWrench extends ItemCore {
+    public ItemWrench() {
+        super();
+        this.setUnlocalizedName("wrench_flux");
+    }
+}
